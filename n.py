513ef3ef6f547075ba7c11a6f0cc2e13edd1ead0
albert <ast_albert@mail.ru>
2019-11-27 00:09:29 +0300

    second commit

--- n.py
+++ n.py
@@ -1,27 +1,35 @@
-import psycopg2
-conn = psycopg2.connect(dbname='albertastaduran', user='albertastaduran',
-                        password='mypassword', host='localhost')
-cursor = conn.cursor()
-def isint(s):
+# import psycopg2
+# conn = psycopg2.connect(dbname='albertastaduran', user='albertastaduran',
+#                         password='mypassword', host='localhost')
+# cursor = conn.cursor()
+# def isint(s):
+#     try:
+#         int(s)
+#         return True
+#     except ValueError:
+#         return False
+# cursor.execute('SELECT name, price FROM product')
+# name_price = cursor.fetchall()
+# # name_price = sum(name_price,())
+# # name_price = list(name_price)
+# # name = []
+# # prices = []
+# # for value in name_price:
+# #     if isint(value) :
+# #         prices.append(value)
+# #     else :
+# #         name.append(value)
+# # for n, row in enumerate(name):
+# #     name[n] = row.replace('\n','')
+
+
+# print(name_price)
+# # print(prices)
+def params(com,index):
     try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-cursor.execute('SELECT name, price FROM product')
-name_price = cursor.fetchall()
-# name_price = sum(name_price,())
-# name_price = list(name_price)
-# name = []
-# prices = []
-# for value in name_price:
-#     if isint(value) :
-#         prices.append(value)
-#     else :
-#         name.append(value)
-# for n, row in enumerate(name):
-#     name[n] = row.replace('\n','')
+        return com[index]
+    except:
+        return None
 
-
-print(name_price)
-# print(prices)
+param = params(all,1)
+print(param)
